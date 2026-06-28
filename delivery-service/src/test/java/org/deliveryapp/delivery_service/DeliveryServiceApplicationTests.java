e86358c8f67c12ec50eddea9bf09af6e27fcf46b
alejoalbornoz <alejoalbornoz912@gmail.com>
2026-06-28 16:59:06 -0300

    Feat: Moved folders from order-service and create delivery-service

---
+++ delivery-service/src/test/java/org/deliveryapp/delivery_service/DeliveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.deliveryapp.delivery_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
